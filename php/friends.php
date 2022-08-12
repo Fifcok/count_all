863af5f1de1d18daf1affc02f68aed476a2cdb64
Fifcok <105712834+Fifcok@users.noreply.github.com>
2022-08-12 18:06:06 +0200

    add all beautify

--- php/friends.php
+++ php/friends.php
@@ -1,25 +1,19 @@
 <?php
-
-echo "
-<div class='col-md-12 text-center'>OkiemDrona
-
-<a href='https://www.youtube.com/channel/UCNmh-TbpICf2r4HOsFSiHeQ' target='_blank'><img class='col-md-12 image_my' src='img/okiem_drona.png'></a></div>
-
+echo "<div class='col-md-12 text-center'>OkiemDrona
+  <a href='https://www.youtube.com/channel/UCNmh-TbpICf2r4HOsFSiHeQ' target='_blank'><img class='col-md-12 image_my' src='img/okiem_drona.png'></a>
+</div>
 
 <div class='col-md-12 text-center'>Virtual Airline
-
-<a href='https://vms.top-sky.eu/' target='_blank'><img class='col-md-12 image_my' src='img/topsky.png'></a></div>
-
+  <a href='https://vms.top-sky.eu/' target='_blank'><img class='col-md-12 image_my' src='img/topsky.png'></a>
+</div>
 
 <div class='col-md-12 text-center'>Yotube channel
-
-<a href='https://www.youtube.com/c/topski87' target='_blank'><img class='col-md-12 image_my' src='img/topsky_yt.png' ></a></div>
-
+  <a href='https://www.youtube.com/c/topski87' target='_blank'><img class='col-md-12 image_my' src='img/topsky_yt.png'></a>
+</div>
 
 <div class='col-md-12 text-center'>Virtual sky
-
-<a href='https://www.vatsim.net/' target='_blank'><img class='col-md-12 image_my' src='img/vatsim.png' ></a></div>
+  <a href='https://www.vatsim.net/' target='_blank'><img class='col-md-12 image_my' src='img/vatsim.png'></a>
+</div>
 
 <br /> <br />";
-
 ?>
