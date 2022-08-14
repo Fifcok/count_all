26c3e2eb02329a875430a842fa13cc41beed10a3
Fifcok <105712834+Fifcok@users.noreply.github.com>
2022-08-14 15:49:59 +0200

    Fix ICAO codes of aircrafts

--- aircrafts.php
+++ aircrafts.php
@@ -38,32 +38,32 @@
 						<div class="row" id="calc_form">
 							<select type="text" name="typ" id="typ">
 								<optgroup label="Boeing">
-									<option value="B738">B737-800</option>
+									<option value="B738">B737-800 (B738)</option>
 								</optgroup>
 								<optgroup label="Airbus">
-									<option value="A221">A220-100</option>
-									<option value="A223">A220-300</option>
-									<option value="A306">A300-600</option>
-									<option value="A313">A310-300</option>
-									<option value="A318">A318</option>
-									<option value="A319">A319</option>
-									<option value="A19N">A319-NEO</option>
-									<option value="A320">A320-200</option>
-									<option value="A20N">A320-NEO</option>
-									<option value="A321">A321-200</option>
-									<option value="A21N">A321-NEO</option>
-									<option value="A332">A330-200</option>
-									<option value="A333">A330-300</option>
-									<option value="A338">A330-800 NEO</option>
-									<option value="A339">A330-900 NEO</option>
-									<option value="A342">A340-200</option>
-									<option value="A343">A340-300X</option>
-									<option value="A345">A340-500</option>
-									<option value="A346">A340-600</option>
-									<option value="A358">A350-800</option>
-									<option value="A359">A350-900</option>
-									<option value="A35K">A350-1000</option>
-									<option value="A388">A380-800</option>
+									<option value="A221">A220-100 (A221)</option>
+									<option value="A223">A220-300 (A223)</option>
+									<option value="A306">A300-600 (A306)</option>
+									<option value="A313">A310-300 (A313)</option>
+									<option value="A318">A318 (A318)</option>
+									<option value="A319">A319 (A319)</option>
+									<option value="A19N">A319-NEO (A19N)</option>
+									<option value="A320">A320-200 (A320)</option>
+									<option value="A20N">A320-NEO (A20N)</option>
+									<option value="A321">A321-200 (A321)</option>
+									<option value="A21N">A321-NEO (A21N)</option>
+									<option value="A332">A330-200 (A332)</option>
+									<option value="A333">A330-300 (A333)</option>
+									<option value="A338">A330-800 NEO (A338)</option>
+									<option value="A339">A330-900 NEO (A339)</option>
+									<option value="A342">A340-200 (A342)</option>
+									<option value="A343">A340-300X (A343)</option>
+									<option value="A345">A340-500 (A345)</option>
+									<option value="A346">A340-600 (A346)</option>
+									<option value="A358">A350-800 (A358)</option>
+									<option value="A359">A350-900 (A359)</option>
+									<option value="A35K">A350-1000 (A35K)</option>
+									<option value="A388">A380-800 (A388)</option>
 								</optgroup>
 							</select>
 						</div>
